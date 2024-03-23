c5f6a7586b4951da633c1ba2814164a6e2f48a12
jramma <pepe.ramos@mortensen.cat>
2024-03-23 16:30:21 +0100

    slider

---
+++ src/scripts/cat.js
@@ -0,0 +1,27 @@
+var currentIndex = 1;
+
+function currentImage(n) {
+  var i;
+  var images = document.getElementsByClassName("imageClass");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  images[n - 1].style.display = "block";
+  dots[n - 1].className += " active";
+}
+
+// Display the initial image
+currentImage(currentIndex);
+
+// Change to the next image every 3 seconds
+setInterval(function () {
+  currentIndex++;
+  if (currentIndex > document.getElementsByClassName("imageClass").length) {
+    currentIndex = 1;
+  }
+  currentImage(currentIndex);
+}, 3000);
