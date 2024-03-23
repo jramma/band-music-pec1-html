3db6bf3732d7a317ebed54c5346376a3ca27db1a
jramma <pepe.ramos@mortensen.cat>
2024-03-23 13:46:11 +0100

    mobile menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello, world!')
+
+// loading padding effect
+window.addEventListener("load", function () {
+  document.body.classList.add("loaded");
+});
+
+function toggleMenu() {
+    var menu = document.getElementById('mobile-menu');
+    if (menu.style.display === 'none') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
