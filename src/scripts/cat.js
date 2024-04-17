8c88b1a7ee9a83fca450de2b7f59d2251f0d6bad
jramma <pepe.ramos@mortensen.cat>
2024-04-17 14:40:29 +0200

    ✨ resize

--- src/scripts/cat.js
+++ src/scripts/cat.js
@@ -1,24 +1,25 @@
 // loading padding effect
 window.addEventListener("load", function () {
-    document.body.classList.add("loaded");
+  document.body.classList.add("loaded");
 });
 
-window.toggleMenu = function() {
-    var menu = document.getElementById('mobile-menu');
-    if (menu.style.display === 'none') {
-        menu.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-    }
-}
+window.toggleMenu = function () {
+  var menu = document.getElementById("mobile-menu");
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+};
 
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
-  effect:"cards",
+  effect: "cards",
   grabCursor: true,
   autoplay: {
-    delay: 2500,
+    // aumentar para que vaya más lento
+    delay: 500,
     disableOnInteraction: false,
   },
   pagination: {
@@ -26,7 +27,7 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
   },
 });
