0a5e5d67d8595125499ec3e47aac79a4c3813f96
jramma <pepe.ramos@mortensen.cat>
2024-05-20 21:47:19 +0200

    tailwind

---
+++ src/main.js
@@ -0,0 +1,79 @@
+window.addEventListener("load", function () {
+  document.body.classList.add("loaded");
+});
+
+window.toggleMenu = function () {
+  var menu = document.getElementById("mobile-menu");
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+};
+
+const canvasAnimation = document.getElementById("audio");
+if (canvasAnimation) {
+  window.onload = function () {
+    var audio = document.getElementById("audio");
+    audio.volume = 0.1;
+    var isPlaying = false;
+
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var ctx = canvas.getContext("2d");
+
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
+
+    src.connect(analyser);
+    analyser.connect(context.destination);
+
+    analyser.fftSize = 256;
+
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+
+    function renderFrame() {
+      requestAnimationFrame(renderFrame);
+
+      x = 0;
+
+      analyser.getByteFrequencyData(dataArray);
+
+      ctx.clearRect(0, 0, WIDTH, HEIGHT); // Limpia el canvas en lugar de rellenarlo con un color
+
+      for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+
+        var r = barHeight + 25 * (i / bufferLength);
+        var g = 250 * (i / bufferLength);
+        var b = 50;
+
+        ctx.fillStyle = "rgb(255,165,0)";
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+        x += barWidth + 1;
+      }
+    }
+
+    document.getElementById("title").addEventListener("click", function () {
+      if (isPlaying) {
+        audio.pause();
+        isPlaying = false;
+      } else {
+        audio.play();
+        renderFrame();
+        isPlaying = true;
+      }
+    });
+  };
+}
