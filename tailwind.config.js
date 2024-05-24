ab349e029320e1f6c7f249e86dfedab1fadfd5d4
jramma <pepe.ramos@mortensen.cat>
2024-05-24 13:44:04 +0200

    🧺 category

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          DEFAULT: "#1E9DFC",
+          dark: "#4C80A7",
+        },
+        brown: {
+          default: "#BD8D46",
+          dark: "#7D6A4D",
+        },
+        orange: "#FCA41E",
+        gray: "#414B52",
+      },
+      fontFamily: {
+        'roboto-mono': ['"Roboto Mono"', 'monospace'],
+        'urbanist': ['"Urbanist"', 'sans-serif'],
+      },
+    },
   },
-  plugins: [
-   
-  ],
-}
+  plugins: [],
+};
