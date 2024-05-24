26b19b5d1dfc9ca5ecb22a221a6ad7ee53d43f35
jramma <pepe.ramos@mortensen.cat>
2024-05-24 16:58:32 +0200

    presentation

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ window.toggleMenu = function () {
 };
 
 const canvasAnimation = document.getElementById("audio");
+
 if (canvasAnimation) {
   window.onload = function () {
     var audio = document.getElementById("audio");
@@ -76,4 +77,22 @@ if (canvasAnimation) {
       }
     });
   };
+}
+
+const links_naranjas = document.getElementById("links_naranjas");
+if (links_naranjas) {
+  links_naranjas.addEventListener("scroll", function () {
+    var middle = window.innerHeight / 2;
+    var elements = document.querySelectorAll("li");
+
+    elements.forEach(function (element) {
+      var rect = element.getBoundingClientRect();
+
+      if (rect.top <= middle && rect.bottom >= middle) {
+        element.style.color = "#fca41e";
+      } else {
+        element.style.color = "white";
+      }
+    });
+  });
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,19 @@ module.exports = {
         "roboto-mono": ['"Roboto Mono"', "monospace"],
         urbanist: ['"Urbanist"', "sans-serif"],
       },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+  ],
 };
